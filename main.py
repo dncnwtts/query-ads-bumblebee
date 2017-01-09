ac0fde95790d7947848a587443d22e30409396a2
Duncan Watts <dwatts@pha.jhu.edu>
2017-01-09 16:22:41 -0500

    Fixed to deal with new JHU web-scraping restrictions, split output into multiple links

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from lxml import html
 import requests
 
+import urllib2
+url='http://physics-astronomy.jhu.edu/people/graduate-students/'
 # Follows http://docs.python-guide.org/en/latest/scenarios/scrape/ to scrape URL
 # of authors, in this case JHU Physics and Astronomy graduate students
-page = requests.get('http://physics-astronomy.jhu.edu/people/graduate-students/')
-tree = html.fromstring(page.content)
+#page = requests.get(url)
+#tree = html.fromstring(page.content)
+
+req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+con = urllib2.urlopen(req)
+tree = con.read()
 
 # List of names, including "Last name, Firstname" occasionally with added
 # nickname. 
-fullnames = tree.xpath('//td[@class="column-1"]/text()')
+#fullnames = tree.xpath('//td[@class="column-1"]/text()')
+fullnames = tree.split('<td class="column-1">')[1:-1]
+for i in range(len(fullnames)):
+    fullnames[i] = fullnames[i].split('<')[0]
+    if fullnames[i].find('"') != -1:
+        fullnames[i] = fullnames[i].split('"')[0]
+    #elif fullnames[i].find("'") != -1:
+    #    fullnames[i] = fullnames[i].split("'")[0]
+print(fullnames)
 
 # Makes author name "Last name, First initial"
 lastnames = [student.split(',')[0] + ', ' + student.split(',')[1][1] for student
@@ -21,13 +35,18 @@ affil = '&q=((pos(aff%3A"Hopkins",1) AND pos(aff%3A"Physics",1) AND ('
 # Ignores publications before authors were graduate students
 ending = ')) AND year%3A2005-2099)&sort=date desc'
 
-url = beginning + display + affil
+
+urls = [beginning + display + affil]*4
+#url2 = beginning + display + affil
+#url3 = beginning + display + affil
+#url4 = beginning + display + affil
 for i, lastname in enumerate(lastnames):
-    if i == 0:
+    j = i/30
+    if i % 30 == 0:
         author = 'author%3A"^' + lastname + '"'
     else:
         author = ' OR author%3A"^' + lastname + '"'
-    url += author
-url += ending
+    urls[j] += author
+urls = [url + ending for url in urls]
 
-print(url)
+print(urls)
