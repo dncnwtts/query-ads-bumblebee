d457fdb9e57423d139b23999992da1049c5681af
Duncan Watts <dwatts@pha.jhu.edu>
2017-01-10 11:34:05 -0500

    Cleaned up, added some missing affiliations

--- main.py
+++ main.py
@@ -3,49 +3,31 @@ import requests
 
 import urllib2
 url='http://physics-astronomy.jhu.edu/people/graduate-students/'
-# Follows http://docs.python-guide.org/en/latest/scenarios/scrape/ to scrape URL
-# of authors, in this case JHU Physics and Astronomy graduate students
-#page = requests.get(url)
-#tree = html.fromstring(page.content)
-
 req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
 con = urllib2.urlopen(req)
 tree = con.read()
 
 # List of names, including "Last name, Firstname" occasionally with added
 # nickname. 
-#fullnames = tree.xpath('//td[@class="column-1"]/text()')
 fullnames = tree.split('<td class="column-1">')[1:-1]
-for i in range(len(fullnames)):
-    fullnames[i] = fullnames[i].split('<')[0]
-    if fullnames[i].find('"') != -1:
-        fullnames[i] = fullnames[i].split('"')[0]
-    #elif fullnames[i].find("'") != -1:
-    #    fullnames[i] = fullnames[i].split("'")[0]
-print(fullnames)
+names = [n.split(',')[0] + ', ' + n.split(',')[1][1] for n in fullnames]
 
-# Makes author name "Last name, First initial"
-lastnames = [student.split(',')[0] + ', ' + student.split(',')[1][1] for student
-        in fullnames if student.split(',')[0] != '\n']
 beginning = 'https://ui.adsabs.harvard.edu/#search/'
 # Ensures all listed publications are referreed
 display = 'fq={!type%3Daqp v%3D%24fq_property}&fq_property=(property%3Arefereed)'
-# Affiliation of first author is "Hopkins", Physics department
-affil = '&q=((pos(aff%3A"Hopkins",1) AND pos(aff%3A"Physics",1) AND ('
+# Affiliation of first author is "Hopkins", Physics department or IQM
+affil = '&q=((pos(aff%3A"Hopkins",1) AND (pos(aff%3A"Quantum",1) OR pos(aff%3A"Physics",1)) AND ('
 # Ignores publications before authors were graduate students
-ending = ')) AND year%3A2005-2099)&sort=date desc'
+ending = ')) AND year%3A2007-2099)&sort=date desc'
 
-
-urls = [beginning + display + affil]*4
-#url2 = beginning + display + affil
-#url3 = beginning + display + affil
-#url4 = beginning + display + affil
-for i, lastname in enumerate(lastnames):
-    j = i/30
+len_query = 30
+urls = [beginning + display + affil]*(len(fullnames)/len_query + 1)
+for i, name in enumerate(names):
+    j = i/len_query
     if i % 30 == 0:
-        author = 'author%3A"^' + lastname + '"'
+        author = 'author%3A"^' + name + '"'
     else:
-        author = ' OR author%3A"^' + lastname + '"'
+        author = ' OR author%3A"^' + name + '"'
     urls[j] += author
 urls = [url + ending for url in urls]
 
